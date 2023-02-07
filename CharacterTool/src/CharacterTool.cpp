e88c43a5eeea84bd31b198c0a010523633b798ca
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-02-07 16:09:42 +0900

    feat : 플레이어 액터 클래스 구현

---
+++ CharacterTool/src/CharacterTool.cpp
@@ -0,0 +1,65 @@
+#include "CharacterTool.h"
+#include "ComponentSystem.h"
+
+using namespace KGCA41B;
+
+void CharacterTool::OnInit()
+{
+	DINPUT->Init(ENGINE->GetWindowHandle(), ENGINE->GetInstanceHandle());
+
+	LoadResource();
+		
+	ComponentSystem::GetInst()->OnInit(reg_scene);
+
+	//character.Regist(reg_scene);
+	//character.InitComponents();
+	//character.BindComponents(reg_scene);
+
+	sys_render.OnCreate(reg_scene);
+	sys_animation.OnCreate(reg_scene);
+	sys_camera.TargetTag(reg_scene, "Player");
+	sys_camera.OnCreate(reg_scene);
+	sys_input.OnCreate(reg_scene);
+
+	//GUI
+	GUI->AddWidget("main_menu", &gw_main_menu_);
+}
+
+void CharacterTool::OnUpdate()
+{
+	sys_input.OnUpdate(reg_scene);
+	//sys_camera.OnUpdate(reg_scene);
+}
+
+void CharacterTool::OnRender()
+{   
+	sys_animation.OnUpdate(reg_scene);
+	sys_render.OnUpdate(reg_scene);
+
+	// GUI
+	GUI->RenderWidgets();
+
+	switch (gw_main_menu_.msg_)
+	{
+	case MsgType::NONE: break;
+
+	case MsgType::OW_RES_VIEWER:
+	{
+		GwResViewer* gw_res_viewer = new GwResViewer;
+		if (GUI->FindWidget(GWNAME(gw_res_viewer)) == nullptr)
+		{
+			GUI->AddWidget(GWNAME(gw_res_viewer), gw_res_viewer);
+		}
+	}
+	}
+}
+
+void CharacterTool::OnRelease()
+{
+	RESOURCE->Release();
+}
+
+void CharacterTool::LoadResource()
+{
+	RESOURCE->Init("../../Contents/");
+}
