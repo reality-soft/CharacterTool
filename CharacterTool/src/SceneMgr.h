6a5d703d1d23c6e0f56853053548b405f3748757
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-02-18 18:05:40 +0900

    feat: 씬 매니저 구현

---
+++ CharacterTool/src/SceneMgr.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Scene.h"
+
+namespace KGCA41B
+{
+	class SceneMgr
+	{
+		SINGLETON(SceneMgr)
+#define SCENE SceneMgr::GetInst()
+	private:
+		map<string, Scene*> resdic_scene;
+	public:
+		void	PushScene(string id, Scene* scene);
+		Scene* LoadScene(string id);
+	};
+}
+
+
