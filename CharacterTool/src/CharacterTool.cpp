42f9965abf6fbe9c9fe7efe9f0b39bbdc29f3461
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-02 18:27:17 +0900

    chore: 엔진 변경에 따른 변경

--- CharacterTool/src/CharacterTool.cpp
+++ CharacterTool/src/CharacterTool.cpp
@@ -12,12 +12,12 @@ void CharacterTool::OnInit()
 
 	SCENE->PushScene("CharacterTool", this);
 
-	level.CreateLevel(200, 200, 10, 10);
+	level.CreateLevel(3, 100, 50, { 8, 8 });
 	level.vs_id_ = "LevelVS.cso";
 	level.ps_id_ = "LevelPS.cso";
 	level.texture_id = { "Ground.png" };
 
-	KGCA41B::QUADTREE->Init(&level, 4);
+	KGCA41B::QUADTREE->Init(&level);
 
 	// Manager Init
 	DATA->Init("../../Contents/Data");
@@ -28,7 +28,7 @@ void CharacterTool::OnInit()
 	// Component Init
 	ComponentSystem::GetInst()->OnInit(reg_scene);
 
-	//sys_camera_.TargetTag(reg_scene, "Player");
+	sys_camera_.TargetTag(reg_scene, "Player");
 	sys_camera_.OnCreate(reg_scene);
 	sys_render_.OnCreate(reg_scene);
 
@@ -36,22 +36,22 @@ void CharacterTool::OnInit()
 	GUI->AddWidget("MainMenu", new GwMainMenu());
 
 	// Key Settings
-	//EVENT->Subscribe({ DIK_D }, Movements::MoveRight, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_W, DIK_D }, Movements::MoveRightForward, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_S, DIK_D }, Movements::MoveRightBack, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_A }, Movements::MoveLeft, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_W, DIK_A }, Movements::MoveLeftForward, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_S, DIK_A }, Movements::MoveLeftBack, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_W }, Movements::MoveForward, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_S }, Movements::MoveBack, KEY_HOLD);
+	EVENT->Subscribe({ DIK_D }, Movements::MoveRight, KEY_HOLD);
+	EVENT->Subscribe({ DIK_W, DIK_D }, Movements::MoveRightForward, KEY_HOLD);
+	EVENT->Subscribe({ DIK_S, DIK_D }, Movements::MoveRightBack, KEY_HOLD);
+	EVENT->Subscribe({ DIK_A }, Movements::MoveLeft, KEY_HOLD);
+	EVENT->Subscribe({ DIK_W, DIK_A }, Movements::MoveLeftForward, KEY_HOLD);
+	EVENT->Subscribe({ DIK_S, DIK_A }, Movements::MoveLeftBack, KEY_HOLD);
+	EVENT->Subscribe({ DIK_W }, Movements::MoveForward, KEY_HOLD);
+	EVENT->Subscribe({ DIK_S }, Movements::MoveBack, KEY_HOLD);
 
-	//EVENT->Subscribe({ DIK_D }, Movements::Idle, KEY_UP);
-	//EVENT->Subscribe({ DIK_S }, Movements::Idle, KEY_UP);
-	//EVENT->Subscribe({ DIK_W }, Movements::Idle, KEY_UP);
-	//EVENT->Subscribe({ DIK_A }, Movements::Idle, KEY_UP);
+	EVENT->Subscribe({ DIK_D }, Movements::Idle, KEY_UP);
+	EVENT->Subscribe({ DIK_S }, Movements::Idle, KEY_UP);
+	EVENT->Subscribe({ DIK_W }, Movements::Idle, KEY_UP);
+	EVENT->Subscribe({ DIK_A }, Movements::Idle, KEY_UP);
 
-	//EVENT->Subscribe({ DIK_SPACE }, Movements::Fire, KEY_HOLD);
-	//EVENT->Subscribe({ DIK_SPACE }, Movements::Idle, KEY_UP);
+	EVENT->Subscribe({ DIK_SPACE }, Movements::Fire, KEY_HOLD);
+	EVENT->Subscribe({ DIK_SPACE }, Movements::Idle, KEY_UP);
 
 	sys_light_.OnCreate(reg_scene);
 }
@@ -67,8 +67,8 @@ void CharacterTool::OnUpdate()
 
 void CharacterTool::OnRender()
 {   
-	sys_render_.OnUpdate(reg_scene);
 	KGCA41B::QUADTREE->Render();
+	sys_render_.OnUpdate(reg_scene);
 
 	// GUI
 	GUI->RenderWidgets();
