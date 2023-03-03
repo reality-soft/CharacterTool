4da17e0d3792a1d84c0edbd240432cfe9b4db1de
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-03 17:57:51 +0900

    chore: 설정할 캐릭터 정보 수정

--- CharacterTool/src/CharacterData.h
+++ CharacterTool/src/CharacterData.h
@@ -5,12 +5,9 @@ struct CharacterData
 {
 	char character_name[256] = "";
 	string anim_id;
-	int max_frame, cur_frame;
 	string skm_id;
-	string skeleton_id;
 	string vs_id;
-	string texture_id;
-	string ps_id;
+	int x = 0, y = 0, z = 0;
 };
 
 
