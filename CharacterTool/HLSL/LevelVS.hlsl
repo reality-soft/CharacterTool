5aa2127d0ec46e3df205a2baf6c5ac182ef0f6f6
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-10 14:53:55 +0900

    feat: 쉐이더 파일 추가 및 수정
    - 레벨 렌더링을 위한 추가

--- CharacterTool/HLSL/LevelVS.hlsl
+++ CharacterTool/HLSL/LevelVS.hlsl
@@ -3,35 +3,32 @@
 struct VS_IN
 {
     float3 p : F3_POSITION;
-    float3 n : F3_NORMAL;
-    float4 c : F4_COLOR;
     float2 t : F2_TEXTURE;
 };
 
 struct VS_OUT
 {
-    float4 p : SV_POSITION;
-    float4 n : NORMAL;
-    float4 c : COLOR;
+    float4 p : SV_POSITION;  
     float2 t : TEXCOORD;
     float lod : TEXCOORD1;
+    
+    float3 view_dir : TEXCOORD2;
+    matrix view_proj : TEXCOORD3;
 };
 
 VS_OUT VS(VS_IN input)
 {
-    VS_OUT output = (VS_OUT) 0;    
+    VS_OUT output = (VS_OUT) 0;
     
-    float4 local = float4(input.p, 1.0f);
-    float4 normal = float4(input.n, 1.0f);
-    
+    float4 local = float4(input.p, 1.0f);    
     float4 world = mul(local, IdentityMatrix());
-    float4 projection = mul(world, ViewProjection());
 
-    output.lod = GetLod(input.p);
-    output.p = projection;
-    output.n = normal;
-    output.c = input.c;
+    output.p = world;
     output.t = input.t;
+    output.lod = GetLod(input.p);
+    
+    output.view_proj = ViewProjection();
+    output.view_dir = (camera_world - world).xyz;
 
     return output;
 }
