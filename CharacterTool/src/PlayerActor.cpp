206ac7516f7dd73d5a7ac7bd33e7d85cfb18ab48
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-14 12:34:39 +0900

    feat: 플레이어의 움직임 형태 변경
    - 카메라가 보는 방향으로 움직이도록 변경

--- CharacterTool/src/PlayerActor.cpp
+++ CharacterTool/src/PlayerActor.cpp
@@ -23,9 +23,8 @@ void reality::PlayerActor::OnInit(entt::registry& registry)
 	bounding_box.SetXYZ(0, 0, 0);
 	registry.emplace<reality::C_BoundingBox>(entity_id_, bounding_box);
 
-
 	C_Camera camera;
-	camera.local = XMMatrixTranslationFromVector({ 0, 20, -40, 0 });
+	camera.local = XMMatrixTranslationFromVector({ 0, 70, -90, 0 });
 	camera.near_z = 1.f;
 	camera.far_z = 10000.f;
 	camera.fov = XMConvertToRadians(45);
@@ -41,6 +40,13 @@ void reality::PlayerActor::OnInit(entt::registry& registry)
 
 void reality::PlayerActor::OnUpdate()
 {
+	C_Camera* camera = reg_scene_->try_get<C_Camera>(entity_id_);
+	XMVECTOR scale, rotation, translation;
+	XMMatrixDecompose(&scale, &rotation, &translation, transform_matrix_);
+	XMMATRIX rotation_matrix = XMMatrixRotationY(camera->pitch_yaw.y);
+	transform_tree_.root_node->Rotate(*reg_scene_, entity_id_, translation, rotation_matrix);
+	front_ = XMVector3Transform({ 0, 0, 1, 0 }, rotation_matrix);
+	right_ = XMVector3Transform({ 1, 0, 0, 0 }, rotation_matrix);
 }
 
 void reality::PlayerActor::SetCharacterData(CharacterData data)
@@ -55,11 +61,11 @@ void reality::PlayerActor::SetCharacterData(CharacterData data)
 
 	reality::C_SkeletalMesh skm;
 	skm.skeletal_mesh_id = data.skm_id;
-	skm.local = XMMatrixIdentity() * XMMatrixRotationY(XMConvertToRadians(180)) * XMMatrixScalingFromVector({ 0.1, 0.1, 0.1, 0.0 });
+	skm.local = XMMatrixIdentity() * XMMatrixRotationY(XMConvertToRadians(180)) * XMMatrixScalingFromVector({ 0.3, 0.3, 0.3, 0.0 });
 	skm.vertex_shader_id = data.vs_id;
 	reg_scene_->emplace_or_replace<reality::C_SkeletalMesh>(entity_id_, skm);
 
-	transform_tree_.root_node->ApplyMovement(*reg_scene_, entity_id_, transform_matrix_);
+	transform_tree_.root_node->Translate(*reg_scene_, entity_id_, transform_matrix_);
 }
 
 void reality::PlayerActor::SetCharacterAnimation(string anim_id)
@@ -72,60 +78,53 @@ void reality::PlayerActor::SetCharacterAnimation(string anim_id)
 void reality::PlayerActor::MoveRight()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_RF_Anim.anim");
-	movement_component_->direction.m128_f32[0]++;
+	movement_component_->direction += right_;
 }
 
 void reality::PlayerActor::MoveRightForward()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_RF_Anim.anim");
-	movement_component_->direction.m128_f32[2]++;
-	movement_component_->direction.m128_f32[0]++;
-
-	movement_component_->direction = XMVector3Normalize(movement_component_->direction);
+	movement_component_->direction += front_;
+	movement_component_->direction += right_;
 }
 
 void reality::PlayerActor::MoveRightBack()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_RB_Anim.anim");
-	movement_component_->direction.m128_f32[2]--;
-	movement_component_->direction.m128_f32[0]++;
-
-	movement_component_->direction = XMVector3Normalize(movement_component_->direction);
+	movement_component_->direction -= front_;
+	movement_component_->direction += right_;
 }
 
 void reality::PlayerActor::MoveLeft()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_LF_Anim.anim");
-	movement_component_->direction.m128_f32[0]--;
+	movement_component_->direction -= right_;
 }
 
 void reality::PlayerActor::MoveLeftForward()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_LF_Anim.anim");
-	movement_component_->direction.m128_f32[2]++;
-	movement_component_->direction.m128_f32[0]--;
-
-	movement_component_->direction = XMVector3Normalize(movement_component_->direction);
+	movement_component_->direction += front_;
+	movement_component_->direction -= right_;
 }
 
 void reality::PlayerActor::MoveLeftBack()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_LB_Anim.anim");
-	movement_component_->direction.m128_f32[2]--;
-	movement_component_->direction.m128_f32[0]--;
-	movement_component_->direction = XMVector3Normalize(movement_component_->direction);
+	movement_component_->direction -= front_;
+	movement_component_->direction -= right_;
 }
 
 void reality::PlayerActor::MoveForward()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_F_Anim.anim");
-	movement_component_->direction.m128_f32[2]++;
+	movement_component_->direction += front_;
 }
 
 void reality::PlayerActor::MoveBack()
 {
 	SetCharacterAnimation("A_TP_CH_Jog_B_Anim.anim");
-	movement_component_->direction.m128_f32[2]--;
+	movement_component_->direction -= front_;
 }
 
 void reality::PlayerActor::Idle()
