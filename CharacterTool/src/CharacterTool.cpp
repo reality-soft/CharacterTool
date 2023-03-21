38d9ca4c68d2ea0519d2dd378381dfee8b9b6598
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-21 10:32:50 +0900

    fix: 엔진 업데이트에 따른 수정
    - 입력 이벤트 바인딩 함수명 변경

--- CharacterTool/src/CharacterTool.cpp
+++ CharacterTool/src/CharacterTool.cpp
@@ -30,26 +30,25 @@ void CharacterTool::OnInit()
 	
 	auto character_actor = SCENE_MGR->GetPlayer<PlayerActor>(0);
 	// Key Settings
-	INPUT_EVENT->Subscribe({ DIK_D }, std::bind(&PlayerActor::MoveRight, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_W, DIK_D }, std::bind(&PlayerActor::MoveRightForward, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_S, DIK_D }, std::bind(&PlayerActor::MoveRightBack, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_A }, std::bind(&PlayerActor::MoveLeft, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_W, DIK_A }, std::bind(&PlayerActor::MoveLeftForward, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_S, DIK_A }, std::bind(&PlayerActor::MoveLeftBack, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_W }, std::bind(&PlayerActor::MoveForward, character_actor), KEY_HOLD);
-	INPUT_EVENT->Subscribe({ DIK_S }, std::bind(&PlayerActor::MoveBack, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_D }, std::bind(&PlayerActor::MoveRight, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_W, DIK_D }, std::bind(&PlayerActor::MoveRightForward, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_S, DIK_D }, std::bind(&PlayerActor::MoveRightBack, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_A }, std::bind(&PlayerActor::MoveLeft, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_W, DIK_A }, std::bind(&PlayerActor::MoveLeftForward, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_S, DIK_A }, std::bind(&PlayerActor::MoveLeftBack, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_W }, std::bind(&PlayerActor::MoveForward, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_S }, std::bind(&PlayerActor::MoveBack, character_actor), KEY_HOLD);
 
 	std::function<void()> idle = std::bind(&PlayerActor::Idle, character_actor);
-	INPUT_EVENT->Subscribe({ DIK_D }, idle, KEY_UP);
-	INPUT_EVENT->Subscribe({ DIK_S }, idle, KEY_UP);
-	INPUT_EVENT->Subscribe({ DIK_W }, idle, KEY_UP);
-	INPUT_EVENT->Subscribe({ DIK_A }, idle, KEY_UP);
-	INPUT_EVENT->Subscribe({ DIK_SPACE }, idle, KEY_UP);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_D }, idle, KEY_UP);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_S }, idle, KEY_UP);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_W }, idle, KEY_UP);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_A }, idle, KEY_UP);
 
-	INPUT_EVENT->Subscribe({ DIK_SPACE }, std::bind(&PlayerActor::Fire, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeMouseEvent({ MouseButton::L_BUTTON }, std::bind(&PlayerActor::Fire, character_actor), KEY_HOLD);
+	INPUT_EVENT->SubscribeMouseEvent({ MouseButton::L_BUTTON }, idle, KEY_UP);
 
-
-	INPUT_EVENT->Subscribe({ DIK_1 }, Movements::CameraModeChange, KEY_PUSH);
+	INPUT_EVENT->SubscribeKeyEvent({ DIK_1 }, Movements::CameraModeChange, KEY_PUSH);
 
 	sys_light_.OnCreate(reg_scene_);
 }
